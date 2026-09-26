e2a9896207c8814dcd6479603ae49b8d2d51f1c0
Alex Groza <72754857+Chamosiala@users.noreply.github.com>
2026-09-26 19:54:27 +0300

    change controls

--- scenes/player.gd
+++ scenes/player.gd
@@ -5,7 +5,7 @@ const SPEED = 650.0
 
 
 func _physics_process(delta: float) -> void:
-	var direction := Input.get_axis("player1_up", "player2_down")
+	var direction := Input.get_axis("player1_up", "player1_down")
 	if direction:
 		velocity.y = direction * SPEED
 	else:
